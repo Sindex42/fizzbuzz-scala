a58a631a8dbf36c138dfa251c6623e56ab1d0bc5
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-26 16:40:15 +0000

    #convert returns 'fizz' and 'buzz' for mulitples of 3 and 5 respectively

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,8 +1,8 @@
 object FizzBuzz extends App {
   def convert(x: Int) = {
-    if(x == 3){
+    if(x % 3 == 0){
       "fizz"
-    } else if(x == 5){
+    } else if(x % 5 == 0){
       "buzz"
     } else{
       x
--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -11,8 +11,16 @@ class FizzBuzzSpec extends FunSpec {
       assert(FizzBuzz.convert(3) === "fizz")
     }
 
+    it("returns 'fizz' for a multiple of 3"){
+      assert(FizzBuzz.convert(6) === "fizz")
+    }
+
     it("returns 'buzz' for 5"){
       assert(FizzBuzz.convert(5) === "buzz")
+    }
+
+    it("returns 'buzz' for a multiple of 5"){
+      assert(FizzBuzz.convert(10) === "buzz")
     }
   }
 }
