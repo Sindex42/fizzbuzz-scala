cfef437bc665f16610ac0dd3b13e4482a6910ec8
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-26 16:48:21 +0000

    #convert returns 'fizzbuzz' for multiples of 15

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,10 +1,12 @@
 object FizzBuzz extends App {
   def convert(x: Int) = {
-    if(x % 3 == 0){
-      "fizz"
+    if(x % 3 == 0 && x % 5 == 0){
+      "fizzbuzz"
     } else if(x % 5 == 0){
       "buzz"
-    } else{
+    } else if(x % 3 == 0){
+      "fizz"
+    } else {
       x
     }
   }
--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -3,24 +3,28 @@ import org.scalatest.FunSpec
 class FizzBuzzSpec extends FunSpec {
 
   describe("#convert"){
-    it("returns the input"){
+    it("returns the input") {
       assert(FizzBuzz.convert(4) === 4)
     }
 
-    it("returns 'fizz' for 3"){
+    it("returns 'fizz' for 3") {
       assert(FizzBuzz.convert(3) === "fizz")
     }
 
-    it("returns 'fizz' for a multiple of 3"){
+    it("returns 'fizz' for a multiple of 3") {
       assert(FizzBuzz.convert(6) === "fizz")
     }
 
-    it("returns 'buzz' for 5"){
+    it("returns 'buzz' for 5") {
       assert(FizzBuzz.convert(5) === "buzz")
     }
 
-    it("returns 'buzz' for a multiple of 5"){
+    it("returns 'buzz' for a multiple of 5") {
       assert(FizzBuzz.convert(10) === "buzz")
+    }
+
+    it("returns 'fizzbuzz' for a multiple of 15") {
+      assert(FizzBuzz.convert(15) === "fizzbuzz")
     }
   }
 }
